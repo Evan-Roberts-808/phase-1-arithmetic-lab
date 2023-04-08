8734a21901539316745b9ca7b1a80b88bce9e805
Evan <eroberts1998@icloud.com>
2023-04-08 15:17:39 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
+// Test 1
+const num1 = 2;
+const num2 = 31;
+let multiply = num1 * num2;
 
-// Write your code here
+// Test 2
+let random = Math.ceil(Math.random()*10);
+
+// Test 3
+const num3 = 8;
+const num4 = 2;
+let mod = num3 / num4;
+
+// Test 4
+let max = Math.max(1, 4, 8, 13, 20);
